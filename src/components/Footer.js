2d9df39ac317f7e0d9c1ce3b524bd548a097c271
Tim Boudreaux <tmboudreaux00@gmail.com>
2021-08-12 19:11:19 -0500

    frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+
+import moduleName from 'module'
+import { Container, Col, Row } from 'react-bootstrap'
+const Footer = () => {
+    return (
+            <footer>
+                <Container>
+                    <Row>
+                        <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+                    </Row>
+                </Container>
+            </footer>
+        
+    )
+}
+
+export default Footer
+
