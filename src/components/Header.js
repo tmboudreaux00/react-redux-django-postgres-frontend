72dc0bd5ebf68a3182d500244a5157b7c3110e1f
Tim Boudreaux <tmboudreaux00@gmail.com>
2021-08-13 21:06:29 -0500

    rating components

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/home">ProShop Logo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i></Nav.Link>
+                        <Nav.Link href="/login"><i className="fa fa-user"></i></Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
